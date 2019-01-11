d31822addc15a2ae2cdfcf0f7fa5dc106bed2ee8
nitish101 <nitish.rao.101@gmail.com>
2019-01-11 15:10:57 +0530

    Did it!

--- Music/urls.py
+++ Music/urls.py
@@ -1,4 +1,4 @@
-#from django.conf.urls import url
+from django.conf.urls import url
 from . import views
 from django.urls import path
 app_name = 'Music'
@@ -7,11 +7,19 @@ app_name = 'Music'
 urlpatterns = [
 
     #/music/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+
+    path('register/', views.UserFormView.as_view(), name='register'),
 
     #/music/<album_id>/
-    path('<int:album_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 
-    #/music/<album_id>/favorite/
-    path('<int:album_id>/favorite/', views.favorite, name='favorite'),
+    path('album/add/', views.AlbumCreate.as_view(), name='album-add'),
+
+    #/music/album/2/
+    path('album/<int:pk>/', views.AlbumUpdate.as_view(), name='album-update'),
+
+    #/music/album/2/delete/
+    path('album/<int:pk>/delete/', views.AlbumDelete.as_view(), name='album-delete'),
+
 ]
